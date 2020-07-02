4640bcdb9393ab130420e37f5429bd5bf224d821
k-iwashita <k.iwashita.gude@gmail.com>
2020-07-03 03:12:42 +0900

    お気に入り機能の実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,3 +1,17 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+
+  def create
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @fav = @user.favorites.create(post_id: @post.id)
+    # redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @fav = current_user.favorites.find_by(post_id: @post.id)
+    @fav.destroy
+    # redirect_back(fallback_location: root_path)
+  end
 end
