dda4fd940482d8295a61a2389c23f11f2c0ca81e
k-iwashita <k.iwashita.gude@gmail.com>
2020-07-02 16:35:09 +0900

    controllerの作成とルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :posts, only: [:create, :destroy, :show, :index] do
+    resource :favorites, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
