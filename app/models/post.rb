ea44459bb8c0ed7f834dbf9833c65e60a8b96561
Aseiide <eiseiide@gmail.com>
2020-07-03 09:36:39 +0900

    first commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ApplicationRecord
   belongs_to :category, optional: true
   has_many :favorites
   attachment :image, destroy: false
+  # titleの文字数は1~10文字まで
+  validates :title,
+    length: {minimum: 1, maximum: 10}
+  # content空がダメ
+  validates :content,
+    presence: true
 end
