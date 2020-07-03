823addd967187522450b55854658c6d1d4de1362
k-iwashita <k.iwashita.gude@gmail.com>
2020-07-03 15:52:57 +0900

    sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   private 
     def after_sign_in_path_for(resource)
-        root_path
+      root_path
     end
 
     def after_sign_out_path_for(resource)
-      root_path
+      new_user_session_path
     end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!
   require "net/http"
   def top
     @categories = Category.all
